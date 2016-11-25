d6ff6ce7b121dd198ed040966990b47aed51f748
Alex Joel <alex-sajamajc@hotmail.com>
2016-11-24 21:38:11 -0400

    modificando Bienvenido

--- view/bienvenido.php
+++ view/bienvenido.php
@@ -15,10 +15,10 @@
 						<img alt="Carousel Bootstrap First" src="http://lorempixel.com/output/sports-q-c-1600-500-1.jpg" />
 						<div class="carousel-caption">
 							<h4>
-								First Thumbnail label
+								Aqui colocas un subtitulo
 							</h4>
 							<p>
-								Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, quod quasi minima? Praesentium facilis ducimus tempore ullam repudiandae dolorem labore fugit at. Cum, quod repellat esse iste, dolores accusantium alias.
 							</p>
 						</div>
 					</div>
