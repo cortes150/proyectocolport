3495efe6cb7240a137603cea72a79ee35187e1f8
Alex Joel <alex-sajamajc@hotmail.com>
2016-11-28 21:37:24 -0400

    buscador compañia

--- view/compania/compania.php
+++ view/compania/compania.php
@@ -1,44 +1,4 @@
-<script type="text/javascript">
-(function(document) {
-  'use strict';
-
-  var LightTableFilter = (function(Arr) {
-
-    var _input;
-
-    function _onInputEvent(e) {
-      _input = e.target;
-      var tables = document.getElementsByClassName(_input.getAttribute('data-table'));
-      Arr.forEach.call(tables, function(table) {
-        Arr.forEach.call(table.tBodies, function(tbody) {
-          Arr.forEach.call(tbody.rows, _filter);
-        });
-      });
-    }
-
-    function _filter(row) {
-      var text = row.textContent.toLowerCase(), val = _input.value.toLowerCase();
-      row.style.display = text.indexOf(val) === -1 ? 'none' : 'table-row';
-    }
-
-    return {
-      init: function() {
-        var inputs = document.getElementsByClassName('light-table-filter');
-        Arr.forEach.call(inputs, function(input) {
-          input.oninput = _onInputEvent;
-        });
-      }
-    };
-  })(Array.prototype);
-
-  document.addEventListener('readystatechange', function() {
-    if (document.readyState === 'complete') {
-      LightTableFilter.init();
-    }
-  });
-
-})(document);
-</script>
+<script src="assets/js/buscador.js"></script>
 <h1>Lista de Campañas</h1>
 <div class="well well-sm text-right">
     <a class="btn btn-success" href="?c=Compania&a=Crear">Agregar Campañas</a>
